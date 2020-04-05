7f98134a1a015ea5a6498093a800dfe170cd3518
Giovanni <cascafico@gmail.com>
2020-04-05 11:29:17 +0200

    Create notes.js

---
+++ notes.js
@@ -0,0 +1,44 @@
+$( document ).ready(function(){
+  $('#form').submit(function(event){
+    event.preventDefault();
+    // non permette di inviare i dati se l'accuratezza supera i 35m
+    if (stato==0) alert("Attendere che l'accuratezza diventi minore di 35m (Testo da Rosso diventi Giallo o
+ preferibilmente Verde)");
+		    else {
+		      
+		      // Verifica Dei Campi
+		      if($("#strada").val()=="") {alert("Il Campo Nome non Può essere vuoto");return false;}
+		      if($("#civico").val()=="") {alert("Il Campo numero civico non può essere vuoto");return fal
+se;}
+		      
+		      //Creazione testo da inserire nella nota
+		      var testo="addr:street="+$("#strada").val()
+			+"\naddr:housenumber="+$("#civico").val()
+			+"\naddr:city="+$("#comune").val()
+			+"\naddr:postcode="+$("#cap").val()
+			+"\ndescription="+$("#note").val();
+		      
+		      // Invia i dati a osm 
+		      $.ajax({
+			type: "POST",
+     	// url: "https://api.openstreetmap.org/api/0.6/notes",  sandbox follow
+			url: "https://api06.dev.openstreetmap.org/api/notes",     
+			data: "lat=" + lat  + "&lon=" + lon +"&text="+testo,
+			dataType: "html",
+			success: function(msg)
+			{
+			  $("#risultato").html(msg);
+			},
+			error: function()
+			{
+			  alert("Chiamata fallita, si prega di riprovare...");
+			}
+		      });
+		      
+		      alert("Dati Inviati \n" + testo);
+		      $('#form')[0].reset();
+		    }
+    return false
+  });
+  
+});  
