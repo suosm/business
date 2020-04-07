5adda9a0fa9b679956e82dd685863c9c5941f5f8
naposm <63296731+naposm@users.noreply.github.com>
2020-04-07 18:57:15 +0200

    Nuovo design

--- geo.js
+++ geo.js
@@ -3,8 +3,6 @@ var lat;
 var lon;
 var direzione;
 
-$( document ).ready(
-  
   //Recupera le coordinate dal device
   function getLocation()
   {
@@ -14,23 +12,22 @@ $( document ).ready(
     }
     else{alert("Questo device non Supporta la Geolocalizzazione");}
   }
-);
-  
+
 function showError()
 {
   alert("Errore nella localizzazione");
 }
-  
+
   //Visualizza la posizione
 function showPosition(position)
   {
   lat=position.coords.latitude;
   lon=position.coords.longitude;
   direzione=0;
-   
+
   //visualizza i dati nel div con id geo
   $("#geo").html("Lat: " + lat + " Lon: " + lon +" Accuratezza: "+ position.coords.accuracy+ "m Direzione: ");
-  
+
   // Gestisce i tre stati rispetto all'accuratezza
   if (position.coords.accuracy>35) { $("#geo").css("color","red"); stato=0;}
   if (position.coords.accuracy<=35 && position.coords.accuracy>20) { $("#geo").css("color","yellow");stato=1;}
--- notes.js
+++ notes.js
@@ -1,14 +1,14 @@
-$( document ).ready(function(){
+function(){
   $('#form').submit(function(event){
     event.preventDefault();
     // non permette di inviare i dati se l'accuratezza supera i 35m
     if (stato==0) alert("Attendere che l'accuratezza diventi minore di 35m (Testo da Rosso diventi Giallo o preferibilmente Verde)");
 		    else {
-		      
+
 		      // Verifica Dei Campi
 		      if($("#strada").val()=="") {alert("Il Campo Nome non Può essere vuoto");return false;}
 		      if($("#civico").val()=="") {alert("Il Campo numero civico non può essere vuoto");return false;}
-		      
+
 		      //Creazione testo da inserire nella nota
 		      var testo="AggiuntoIndirizzo"
 		        +"\naddr:street="+$("#strada").val()
@@ -16,9 +16,9 @@ $( document ).ready(function(){
 			+"\naddr:city="+$("#comune").val()
 			+"\naddr:postcode="+$("#cap").val()
 			+"\ndescription="+$("#note").val();
-		      
-		      // Invia i dati a osm 
-		      // https://api.openstreetmap.org/api/0.6/notes?lat=51.00&lon=0.1&text=ThisIsANote		      
+
+		      // Invia i dati a osm
+		      // https://api.openstreetmap.org/api/0.6/notes?lat=51.00&lon=0.1&text=ThisIsANote
 		      $.ajax({
 			type: "POST",
      	                url: "https://api.openstreetmap.org/api/0.6/notes?",
@@ -33,11 +33,11 @@ $( document ).ready(function(){
 			  alert("Chiamata fallita, si prega di riprovare...");
 			}
 		      });
-		      
+
 		      alert("Dati Inviati \n" + testo);
 		      $('#form')[0].reset();
 		    }
     return false
   });
-  
-});  
+
+}
